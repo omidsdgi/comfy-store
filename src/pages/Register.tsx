bee99ebe93dc17b96fdd8a792fe8d93a52879701
omidsdgi <omid69sdgi@gmail.com>
2026-02-12 18:35:48 +0330

    refactor(Register): improve component structure

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,22 @@
+import {Form, Link} from "react-router-dom";
+import {FormInput, SubmitBtn} from "../components";
+
 const Register = () => {
     return (
-        <h1 className="text-4xl">
-            Register
-        </h1>
+        <section className='grid place-items-center h-screen'>
+            <Form className='card w-96  bg-base-100 shadow-lg flex flex-col gap-y-4'>
+                <h4 className='text-center text-3xl font-bold'>Register</h4>
+                <FormInput name='username' label='usertname'  type='text'/>
+                <FormInput name='email' label='email'  type='email'/>
+                <FormInput name='password' label='password'  type='password'/>
+                <div className="mt-4">
+                    <SubmitBtn text="Register"/>
+                </div>
+                <p className='text-center'>Already a member?
+                <Link to='/login' className=' ml-2 link-hover link-primary capitalize'> Login </Link>
+                </p>
+            </Form>
+        </section>
     );
 };
 
