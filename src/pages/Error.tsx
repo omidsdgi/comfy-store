86762e54427a96237ae523d5d32f87d68217bbd7
omidsdgi <omid69sdgi@gmail.com>
2026-02-12 10:35:06 +0330

    feat: add type-safe ErrorPage with custom 404 and fallback UI

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,8 +1,29 @@
+import {isRouteErrorResponse, Link, useRouteError} from "react-router-dom";
+
 const Error = () => {
+    const error = useRouteError();
+    if (isRouteErrorResponse(error)) {
+        if (error.status === 404) {
+            return (
+                <main className="grid min-h-screen place-items-center px-8">
+                    <div className="text-center">
+                        <p className="text-primary font-semibold text-9xl">404</p>
+                        <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text-5xl'>page not found</h1>
+                        <p className='mt-6 text-lg leading-7'>Sorry, we couldn't find the page you're looking for.</p>
+                        <div className='mt-10'>
+                            <Link to='/' className='btn btn-secondary'>go back home</Link>
+                        </div>
+                    </div>
+                </main>
+            )
+        }
+    }
     return (
-        <h1 className="text-4xl">
-            Error
-        </h1>
+        <main className="grid min-h-screen place-items-center px-8">
+            <h4 className="text-center font-bold text-4xl">
+                there was an error...
+            </h4>
+        </main>
     );
 };
 
