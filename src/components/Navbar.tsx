95ca49d2cff1d95259d42237e1a267c005de3084
omidsdgi <omid69sdgi@gmail.com>
2026-02-14 19:59:20 +0330

    feat(NavLinks): add basic structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import {NavLink} from "react-router-dom";
 import {FaBarsStaggered} from "react-icons/fa6";
 import {BsCart} from "react-icons/bs";
+import NavLinks from "./NavLinks.tsx";
 
 const Navbar = () => {
     return (
@@ -12,14 +13,14 @@ const Navbar = () => {
                         <label tabIndex={0} className='btn btn-ghost lg:hidden'>
                             <FaBarsStaggered className="h-6 w-6"/>
                         </label>
-                        <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200">
-                            nav links
+                        <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52">
+                            <NavLinks/>
                         </ul>
                     </div>
                 </div>
 
                 <div className="navbar-center hidden lg:flex">
-                    <ul className='menu menu-horizontal'> nav Link</ul>
+                    <ul className='menu menu-horizontal'> <NavLinks/></ul>
                 </div>
                 <div className="navbar-end"></div>
                 <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
