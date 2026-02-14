638eeee7cf30008adacaaf966ae8c4ec33f0c3c6
omidsdgi <omid69sdgi@gmail.com>
2026-02-14 20:09:21 +0330

    chore(types): add LinkType interface

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,23 +1,29 @@
 import {NavLink} from "react-router-dom";
+interface LinkType {
+    id: number;
+    url: string;
+    text: string;
+}
 
-const links= [
+const links:LinkType[]= [
     { id: 1, url: '/', text: 'home' },
-    { id: 2, url: 'about', text: 'about' },
-    { id: 3, url: 'products', text: 'products' },
-    { id: 4, url: 'cart', text: 'cart' },
-    { id: 5, url: 'checkout', text: 'checkout' },
-    { id: 6, url: 'orders', text: 'orders' },
+    { id: 2, url: '/about', text: 'about' },
+    { id: 3, url: '/products', text: 'products' },
+    { id: 4, url: '/cart', text: 'cart' },
+    { id: 5, url: '/checkout', text: 'checkout' },
+    { id: 6, url: '/orders', text: 'orders' },
 ];
 
 const NavLinks = () => {
     return (
         <>
-            {links.map((link) => {
-                const{ id, url,text } = link;
-                return (<li key={id}>
-                    <NavLink to={url} className='capitalize'>{text}</NavLink>
-                </li>)
-            })}
+            {links.map(({id, url,text}) => (
+                <li key={id}>
+                    <NavLink to={url} className='capitalize'>
+                        {text}
+                    </NavLink>
+                </li>
+                ))}
         </>
     );
 };
