c2fda47d681195b7cfd275c7c172f85ffad17775
omidsdgi <omid69sdgi@gmail.com>
2026-02-15 18:04:29 +0330

    feat(about): complete about page layout and content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,20 @@
 const About = () => {
     return (
-        <h1 className="text-4xl">
-            About us
+        <>
+        <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center' >
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-4xl ">
+            We love
         </h1>
+            <div className="stats bg-primary shadow">
+                <div className="stat">
+                    <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+                       comfy
+                    </div>
+                </div>
+            </div>
+        </div>
+            <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'><span className="20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis consequatur excepturi ipsam iure repudiandae sunt. Enim error ipsum magni maxime voluptatem. Assumenda autem cumque enim nobis non, perferendis perspiciatis quia repudiandae saepe similique, ullam, vel voluptatibus? Aliquam deleniti eaque nostrum sunt unde. Delectus, dolore est ipsam iste modi nam nostrum odio sapiente! Incidunt modi perspiciatis rerum, suscipit vero voluptas?    </span></p>
+        </>
     );
 };
 
